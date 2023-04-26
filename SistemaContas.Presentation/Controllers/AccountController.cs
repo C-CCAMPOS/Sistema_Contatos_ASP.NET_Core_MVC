399adcca756cc35882df44743e101f5037e4e3e7
C-CCAMPOS <clebercampos2011@hotmail.com>
2023-04-25 22:56:43 -0300

    First commit - Criando página Login, Registro e Password

---
+++ SistemaContas.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaContas.Presentation.Models.Account;
+
+namespace SistemaContas.Presentation.Controllers
+{
+    public class AccountController : Controller
+    {
+        //GET: /Account/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        //POST: /Account/Login
+        [HttpPost] //Método para receber o SUBMIT POST do formulário
+        public IActionResult Login(LoginViewModel model)
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult PasswordRecover()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SistemaContas.Presentation/Models/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace SistemaContas.Presentation.Models.Account
+{
+    /// <summary>
+    /// Modelo de dados para o formulário de login de usuário
+    /// </summary>
+    public class LoginViewModel 
+    {
+        [EmailAddress(ErrorMessage = "Por favor, informe um endereço de email válido.")]
+        [Required(ErrorMessage = "Por favor, informe seu email de acesso.")]
+
+        public string? Email { get; set; }
+
+        [MinLength(8, ErrorMessage = "Por favor, informe no minimo {1} caracteres.")]
+        [MaxLength(20, ErrorMessage = "Por favor, informe no máximo {1} caracteres.")]
+        [Required(ErrorMessage = "Por favor, informe sua senha de acesso.")]
+    public string? Senha { get; set; }
+    }
+}
